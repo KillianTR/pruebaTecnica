c180a3408ff52eca061dc6d9e7fd575371149d24
Killian Torrell <161701378+KillianTR@users.noreply.github.com>
2024-07-01 10:37:06 +0200

    Update jsonFalsificado.js

---
+++ js/jsonFalsificado.js
@@ -0,0 +1,29 @@
+const articles = [
+    {
+        id: 1,
+        titular: 'Primer artículo',
+        contenido: 'Este es el contenido del primer artículo.'
+    },
+    {
+        id: 2,
+        titular: 'Segundo artículo',
+        contenido: 'Este es el contenido del segundo artículo.'
+    },
+    {
+        id: 3,
+        titular: 'Tercer artículo',
+        contenido: 'Este es el contenido del tercer artículo.'
+    },
+    {
+        id: 4,
+        titular: 'Cuarto artículo',
+        contenido: 'Este es el contenido del cuarto artículo.'
+    },
+    {
+        id: 5,
+        titular: 'Quinto artículo',
+        contenido: 'Este es el contenido del quinto artículo.'
+    }
+];
+
+console.log(articles);
